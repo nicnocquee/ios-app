5ed15debf4983cd53a3c95bed9e368959b6452eb
jrecio <jrecio@solidgear.es>
2018-06-01 08:41:21 +0200

    add more tests to login suite: checks on credentials basic auth, server name and delete bookmark

--- ownCloudTests/OwnCloudTests.swift
+++ ownCloudTests/OwnCloudTests.swift
@@ -14,6 +14,9 @@ import EarlGrey
 class OwnCloudTests: XCTestCase {
     
     let demoServer: String = "demo.owncloud.org"
+    let username : String = "demo"
+    let password : String = "demo"
+    let nameServer : String = "demo"
     
     override func setUp() {
         super.setUp()
@@ -59,10 +62,25 @@ class OwnCloudTests: XCTestCase {
     }
     
     /*
+     * Passed if: URL with no preffix under http shows SSL connection error
+     */
+    func test4BadProtocol() {
+        
+        EarlGrey.select(elementWithMatcher: grey_accessibilityID("row-url-url")).perform(grey_replaceText(""))
+        EarlGrey.select(elementWithMatcher: grey_accessibilityID("row-continue-continue")).perform(grey_tap())
+        
+        //Asserts
+        
+        EarlGrey.select(elementWithMatcher: grey_text("An SSL error has occurred and a secure connection to the server cannot be made.")).assert(grey_sufficientlyVisible())
+        EarlGrey.select(elementWithMatcher: grey_text("OK")).perform(grey_tap())
+    }
+    
+    
+    /*
      * Passed if: Credentials: username, password, server name, and certificate are displayed if connection works to basic auth server
      */
     
-    func test4CorrectURLShowFields() {
+    func test5CorrectURLShowFields() {
         
         EarlGrey.select(elementWithMatcher: grey_accessibilityID("row-url-url")).perform(grey_replaceText(demoServer))
         EarlGrey.select(elementWithMatcher: grey_accessibilityID("row-continue-continue")).perform(grey_tap())
@@ -75,7 +93,70 @@ class OwnCloudTests: XCTestCase {
         
     }
     
-    func test5PerformanceExample() {
+    
+    /*
+     * Passed if: Username and password hints are visible (we remain in the same view)
+     */
+    func test6EmptyCredentials() {
+        
+        EarlGrey.select(elementWithMatcher: grey_accessibilityID("row-continue-continue")).perform(grey_tap())
+        
+        //Asserts
+        
+        EarlGrey.select(elementWithMatcher: grey_accessibilityLabel("Username")).assert(grey_sufficientlyVisible())
+        EarlGrey.select(elementWithMatcher: grey_accessibilityLabel("Password")).assert(grey_sufficientlyVisible())
+        
+    }
+    
+    /*
+     * Passed if: Username and password hints are visible (user remains in the same view because credentials error)
+     */
+    func test7CorrectLogin() {
+        
+        EarlGrey.select(elementWithMatcher: grey_accessibilityID("row-name-name")).perform(grey_replaceText(nameServer))
+        EarlGrey.select(elementWithMatcher: grey_accessibilityID("row-credentials-username")).perform(grey_replaceText(username))
+        EarlGrey.select(elementWithMatcher: grey_accessibilityID("row-credentials-password")).perform(grey_replaceText(password))
+        EarlGrey.select(elementWithMatcher: grey_accessibilityID("row-continue-continue")).perform(grey_tap())
+        
+        //Asserts
+        
+        EarlGrey.select(elementWithMatcher: grey_accessibilityLabel("Username")).assert(grey_notVisible())
+        EarlGrey.select(elementWithMatcher: grey_accessibilityLabel("Password")).assert(grey_notVisible())
+        EarlGrey.select(elementWithMatcher: grey_text(nameServer)).assert(grey_sufficientlyVisible())
+        
+    }
+    
+    /*
+     * Passed if: Bookmark is not deleted after cancelling the confirmation
+     */
+    func test8CancelDeleteBookmark() {
+        
+        EarlGrey.select(elementWithMatcher: grey_text(nameServer)).perform(grey_swipeFastInDirection(GREYDirection.left))
+        EarlGrey.select(elementWithMatcher: grey_text("Delete")).perform(grey_tap())
+        EarlGrey.select(elementWithMatcher: grey_text("Cancel")).perform(grey_tap())
+        
+        //Asserts
+        
+        EarlGrey.select(elementWithMatcher: grey_text(nameServer)).assert(grey_sufficientlyVisible())
+        
+    }
+    
+    /*
+     * Passed if: Bookmark is deleted, so welcome view to add server is displayed with the "Add Button" enabled
+     */
+    func test9DeleteBookmark() {
+        
+        EarlGrey.select(elementWithMatcher: grey_text(nameServer)).perform(grey_swipeFastInDirection(GREYDirection.left))
+        EarlGrey.select(elementWithMatcher: grey_text("Delete")).perform(grey_tap())
+        EarlGrey.select(elementWithMatcher: grey_text("Delete")).perform(grey_tap())
+        
+        //Asserts
+        
+        EarlGrey.select(elementWithMatcher: grey_accessibilityID("addServer")).assert(grey_enabled())
+        
+    }
+    
+    func test10PerformanceExample() {
         // This is an example of a performance test case.
         self.measure {
             // Put the code you want to measure the time of here.
